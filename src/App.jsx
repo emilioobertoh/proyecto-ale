7f72b695241ace11ff881c22b0a60d6a29b56d3a
alecarpy24 <aleannyp@gmail.com>
2024-11-14 21:39:54 +0100

    Use browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Inicio } from './components/Inicio';
 import { Conciertos } from './components/Conciertos';
-import {Contacto} from './components/Contacto';
+import { Contacto } from './components/Contacto';
 import { Discordia } from './components/Discordia';
 import './App.css';
 
 import { Container } from 'react-bootstrap';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faCheckSquare, faCoffee, faLocationDot, faStar,faAnglesDown,faPhone, faEnvelopeOpenText, faEnvelopesBulk, faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import { faCheckSquare, faCoffee, faLocationDot, faStar, faAnglesDown, faPhone, faEnvelopeOpenText, faEnvelopesBulk, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-library.add(fab, faCheckSquare, faCoffee, faStar, faInstagram, faLocationDot, faAnglesDown, faPhone, faEnvelopeOpenText,faEnvelopesBulk,faEnvelope);
+library.add(fab, faCheckSquare, faCoffee, faStar, faInstagram, faLocationDot, faAnglesDown, faPhone, faEnvelopeOpenText, faEnvelopesBulk, faEnvelope);
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    
     <Router>
-      <Header/>
-      
+      <Header />
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/conciertos" element={<Conciertos />} />
         <Route path="/contacto" element={<Contacto />} />
         <Route path="/discordia" element={<Discordia />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
