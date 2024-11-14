2c193eadb16da61c64ecde932b1be0abcca2bd91
alecarpy24 <aleannyp@gmail.com>
2024-11-14 21:52:41 +0100

    Router changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Inicio } from './components/Inicio';
@@ -11,24 +11,82 @@ import './App.css';
 import { Container } from 'react-bootstrap';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faCheckSquare, faCoffee, faLocationDot, faStar, faAnglesDown, faPhone, faEnvelopeOpenText, faEnvelopesBulk, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCheckSquare,
+  faCoffee,
+  faLocationDot,
+  faStar,
+  faAnglesDown,
+  faPhone,
+  faEnvelopeOpenText,
+  faEnvelopesBulk,
+  faEnvelope,
+} from '@fortawesome/free-solid-svg-icons';
 
-library.add(fab, faCheckSquare, faCoffee, faStar, faInstagram, faLocationDot, faAnglesDown, faPhone, faEnvelopeOpenText, faEnvelopesBulk, faEnvelope);
+library.add(
+  fab,
+  faCheckSquare,
+  faCoffee,
+  faStar,
+  faInstagram,
+  faLocationDot,
+  faAnglesDown,
+  faPhone,
+  faEnvelopeOpenText,
+  faEnvelopesBulk,
+  faEnvelope
+);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <>
+        <Header />
+        <Inicio />
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: '/conciertos',
+    element: (
+      <>
+        <Header />
+        <Conciertos />
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: '/contacto',
+    element: (
+      <>
+        <Header />
+        <Contacto />
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: '/discordia',
+    element: (
+      <>
+        <Header />
+        <Discordia />
+        <Footer />
+      </>
+    ),
+  },
+]);
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/conciertos" element={<Conciertos />} />
-        <Route path="/contacto" element={<Contacto />} />
-        <Route path="/discordia" element={<Discordia />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <Container>
+      <RouterProvider router={router} />
+    </Container>
   );
 }
 
