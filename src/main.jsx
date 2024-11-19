cd8dfb0e774d6d696cd317c4451435ef52f4c83d
emilioobertoh <obertoe.1996@gmail.com>
2024-11-19 19:30:03 +0100

    fixRoutingIssues v1.4

--- src/main.jsx
+++ src/main.jsx
@@ -43,43 +43,44 @@ library.add(
 
 
 /*COMPONENTES*/
-import { Header } from './components/Header.jsx';
-import { Footer } from './components/Footer.jsx';
+// import { Header } from './components/Header.jsx';
+// import { Footer } from './components/Footer.jsx';
 import { Inicio } from './components/Inicio';
 import ErrorPage from "./error-page";
 import { Conciertos } from './components/Conciertos.jsx';
 import { Discordia } from './components/Discordia.jsx';
 import { Contacto } from './components/Contacto.jsx';
+import { Root } from './components/Root.jsx';
 
 const router = createHashRouter([
   {
     path: "/",
-    element: <Inicio />,
+    element: <Root />,
     errorElement: <ErrorPage />,
-  },
-  {
-    path: "/conciertos",
-    element: <Conciertos />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/discordia",
-    element: <Discordia />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/contacto",
-    element: <Contacto/>,
-    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "",
+        element: <Inicio />,
+      },
+      {
+        path: "conciertos",
+        element: <Conciertos />,
+      },
+      {
+        path: "discordia",
+        element: <Discordia />,
+      },
+      {
+        path: "contacto",
+        element: <Contacto />,
+      }
+    ]
   }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <Header />
-      <Footer />
-    </RouterProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 /*
